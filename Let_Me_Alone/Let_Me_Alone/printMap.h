30e0192b404567083c114dd6ad251172bf024747
toyede <toyede@naver.com>
2024-11-10 12:35:57 +0900

    맵 생성 코드 수정
    .

--- Let_Me_Alone/Let_Me_Alone/printMap.h
+++ Let_Me_Alone/Let_Me_Alone/printMap.h
@@ -4,7 +4,7 @@
 #define HEIGHT 7
 
 // 가중치를 가진 격자 출력 함수 선언
-void printGrid(int weights[WIDTH][HEIGHT][4]);
+void printGrid(int weights[HEIGHT][WIDTH][4]);
 
 // 가중치 초기화 함수 선언
 void initializeWeights(int weights[HEIGHT][WIDTH][4]);
